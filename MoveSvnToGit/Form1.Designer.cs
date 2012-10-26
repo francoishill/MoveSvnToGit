7201582b64ce7d2ac6e10bd0496cfff19ff15122
francois <francois@0e69236c-437f-2e46-9ef3-a924fe1e5a0a>
2012-10-26 08:48:10 +0000

    git-svn-id: svn://localhost/movesvntogit/trunk@2 0e69236c-437f-2e46-9ef3-a924fe1e5a0a

---
+++ MoveSvnToGit/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MoveSvnToGit
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBoxSvnUrl = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxLocalGitClonedFolder = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBoxRemoteGitRepo = new System.Windows.Forms.TextBox();
+			this.buttonAccept = new System.Windows.Forms.Button();
+			this.textBoxMessages = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.numericUpDownStartSvnRevisionNumber = new System.Windows.Forms.NumericUpDown();
+			this.label5 = new System.Windows.Forms.Label();
+			this.checkBoxStandardLayout = new System.Windows.Forms.CheckBox();
+			this.checkBoxInitRepo = new System.Windows.Forms.CheckBox();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownStartSvnRevisionNumber)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBoxSvnUrl
+			// 
+			this.textBoxSvnUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxSvnUrl.Location = new System.Drawing.Point(126, 12);
+			this.textBoxSvnUrl.Name = "textBoxSvnUrl";
+			this.textBoxSvnUrl.Size = new System.Drawing.Size(440, 20);
+			this.textBoxSvnUrl.TabIndex = 1;
+			this.textBoxSvnUrl.Tag = "Svn repo url";
+			this.textBoxSvnUrl.Text = "svn://path/to repo   OR   http://path/to/repo   OR   c:\\path\\to\\repo";
+			this.textBoxSvnUrl.Enter += new System.EventHandler(this.textBox_Enter);
+			this.textBoxSvnUrl.Leave += new System.EventHandler(this.textBox_Leave);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(53, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(67, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Svn repo url:";
+			// 
+			// textBoxLocalGitClonedFolder
+			// 
+			this.textBoxLocalGitClonedFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxLocalGitClonedFolder.Location = new System.Drawing.Point(126, 100);
+			this.textBoxLocalGitClonedFolder.Name = "textBoxLocalGitClonedFolder";
+			this.textBoxLocalGitClonedFolder.Size = new System.Drawing.Size(440, 20);
+			this.textBoxLocalGitClonedFolder.TabIndex = 4;
+			this.textBoxLocalGitClonedFolder.Tag = "Local Git folder (cloned)";
+			this.textBoxLocalGitClonedFolder.Text = "c:\\path\\to\\clone\\svn\\code";
+			this.textBoxLocalGitClonedFolder.Enter += new System.EventHandler(this.textBox_Enter);
+			this.textBoxLocalGitClonedFolder.Leave += new System.EventHandler(this.textBox_Leave);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 103);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(122, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Local Git folder (cloned):";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(33, 134);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(87, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Remote Git repo:";
+			// 
+			// textBoxRemoteGitRepo
+			// 
+			this.textBoxRemoteGitRepo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxRemoteGitRepo.Location = new System.Drawing.Point(126, 131);
+			this.textBoxRemoteGitRepo.Name = "textBoxRemoteGitRepo";
+			this.textBoxRemoteGitRepo.Size = new System.Drawing.Size(369, 20);
+			this.textBoxRemoteGitRepo.TabIndex = 5;
+			this.textBoxRemoteGitRepo.Tag = "Remote Git repo";
+			this.textBoxRemoteGitRepo.Text = "git://path/to/git/repo   OR   c:\\path\\to\\git\\repo";
+			this.textBoxRemoteGitRepo.Enter += new System.EventHandler(this.textBox_Enter);
+			this.textBoxRemoteGitRepo.Leave += new System.EventHandler(this.textBox_Leave);
+			// 
+			// buttonAccept
+			// 
+			this.buttonAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonAccept.Location = new System.Drawing.Point(501, 331);
+			this.buttonAccept.Name = "buttonAccept";
+			this.buttonAccept.Size = new System.Drawing.Size(65, 23);
+			this.buttonAccept.TabIndex = 0;
+			this.buttonAccept.Text = "A&ccept";
+			this.buttonAccept.UseVisualStyleBackColor = true;
+			this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+			// 
+			// textBoxMessages
+			// 
+			this.textBoxMessages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBoxMessages.Location = new System.Drawing.Point(12, 164);
+			this.textBoxMessages.Multiline = true;
+			this.textBoxMessages.Name = "textBoxMessages";
+			this.textBoxMessages.ReadOnly = true;
+			this.textBoxMessages.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBoxMessages.Size = new System.Drawing.Size(554, 161);
+			this.textBoxMessages.TabIndex = 6;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(26, 63);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(94, 13);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Start from revision:";
+			// 
+			// numericUpDownStartSvnRevisionNumber
+			// 
+			this.numericUpDownStartSvnRevisionNumber.Location = new System.Drawing.Point(126, 61);
+			this.numericUpDownStartSvnRevisionNumber.Maximum = new decimal(new int[] {
+            -402653185,
+            -1613725636,
+            54210108,
+            0});
+			this.numericUpDownStartSvnRevisionNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDownStartSvnRevisionNumber.Name = "numericUpDownStartSvnRevisionNumber";
+			this.numericUpDownStartSvnRevisionNumber.Size = new System.Drawing.Size(92, 20);
+			this.numericUpDownStartSvnRevisionNumber.TabIndex = 3;
+			this.numericUpDownStartSvnRevisionNumber.Tag = "Start from revision";
+			this.numericUpDownStartSvnRevisionNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(33, 39);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(84, 13);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Standard layout:";
+			// 
+			// checkBoxStandardLayout
+			// 
+			this.checkBoxStandardLayout.AutoSize = true;
+			this.checkBoxStandardLayout.Location = new System.Drawing.Point(126, 38);
+			this.checkBoxStandardLayout.Name = "checkBoxStandardLayout";
+			this.checkBoxStandardLayout.Size = new System.Drawing.Size(231, 17);
+			this.checkBoxStandardLayout.TabIndex = 2;
+			this.checkBoxStandardLayout.Text = "Standard layout   ( trunk / branches / tags )";
+			this.checkBoxStandardLayout.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxInitRepo
+			// 
+			this.checkBoxInitRepo.AutoSize = true;
+			this.checkBoxInitRepo.Location = new System.Drawing.Point(501, 133);
+			this.checkBoxInitRepo.Name = "checkBoxInitRepo";
+			this.checkBoxInitRepo.Size = new System.Drawing.Size(64, 17);
+			this.checkBoxInitRepo.TabIndex = 9;
+			this.checkBoxInitRepo.Text = "Init repo";
+			this.checkBoxInitRepo.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(578, 366);
+			this.Controls.Add(this.checkBoxInitRepo);
+			this.Controls.Add(this.checkBoxStandardLayout);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.numericUpDownStartSvnRevisionNumber);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBoxMessages);
+			this.Controls.Add(this.buttonAccept);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBoxRemoteGitRepo);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBoxLocalGitClonedFolder);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxSvnUrl);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Move SVN to Git";
+			this.Shown += new System.EventHandler(this.Form1_Shown);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDownStartSvnRevisionNumber)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBoxSvnUrl;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxLocalGitClonedFolder;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBoxRemoteGitRepo;
+		private System.Windows.Forms.Button buttonAccept;
+		private System.Windows.Forms.TextBox textBoxMessages;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDownStartSvnRevisionNumber;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.CheckBox checkBoxStandardLayout;
+		private System.Windows.Forms.CheckBox checkBoxInitRepo;
+	}
+}
+
---
+++ MoveSvnToGit/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using SharedClasses;
+using System.Diagnostics;
+
+namespace MoveSvnToGit
+{
+	public partial class Form1 : Form
+	{
+		Dictionary<TextBox, string> textboxesWithInitialWatermarkText = new Dictionary<TextBox, string>();
+
+		/* Inststructions to make use of svn://localhost
+		 * In TortoiseSVN\bin (or Subversion\bin) forlder run svnserve.exe as follows:
+		 * svnserve.exe --daemon --root c:\francois\dev\repos\vsprojects
+		 * then can use svn url (for git-svn):
+		 * svn://localhost/windowsstartupmanager
+		 */
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			textboxesWithInitialWatermarkText.Add(textBoxSvnUrl, textBoxSvnUrl.Text);
+			textboxesWithInitialWatermarkText.Add(textBoxLocalGitClonedFolder, textBoxLocalGitClonedFolder.Text);
+			textboxesWithInitialWatermarkText.Add(textBoxRemoteGitRepo, textBoxRemoteGitRepo.Text);
+
+			//file:///C:/Francois/Dev/repos/vsprojects/windowsstartupmanager/trunk
+			textBoxSvnUrl.Text = @"svn://localhost/windowsstartupmanager/trunk";
+			checkBoxStandardLayout.Checked = false;//Because we only check out the Trunc url we dont use StandardLayout
+			numericUpDownStartSvnRevisionNumber.Value = 1;
+			textBoxLocalGitClonedFolder.Text = @"C:\Francois\Other\tmp\testGit\WindowsStartupManager";
+			textBoxRemoteGitRepo.Text = @"C:\Francois\Other\tmp\testGit\WindowsStartupManager_repo";
+			checkBoxInitRepo.Checked = true;
+
+			/*textBoxSvnUrl.Text = "svn://iserver.gls.co.za/GLS/Version6/GLSCore6";
+			numericUpDownStartSvnRevisionNumber.Value = 3828;
+			textBoxLocalGitClonedFolder.Text = @"C:\ProgrammingGit\test\GLSCore6";
+			textBoxRemoteGitRepo.Text = @"C:\ProgrammingGit\_repos\GLSCore6";*/
+		}
+
+		private const string GitExePath = @"C:\Program Files (x86)\Git\bin\git.exe";
+		private void Form1_Shown(object sender, EventArgs e)
+		{
+			if (!File.Exists(GitExePath))
+			{
+				UserMessages.ShowErrorMessage("Cannot find git EXE (click OK to exit): " + GitExePath);
+				Application.Exit();
+			}
+		}
+
+		private void textBox_Enter(object sender, EventArgs e)
+		{
+			var textboxesKeys = textboxesWithInitialWatermarkText.Keys.ToArray();
+			for (int i = 0; i < textboxesKeys.Length; i++)
+				if (sender == textboxesKeys[i] && textboxesKeys[i].Text == textboxesWithInitialWatermarkText[textboxesKeys[i]])
+					textboxesKeys[i].Text = null;
+		}
+
+		private void textBox_Leave(object sender, EventArgs e)
+		{
+			var textboxesKeys = textboxesWithInitialWatermarkText.Keys.ToArray();
+			for (int i = 0; i < textboxesKeys.Length; i++)
+				if (sender == textboxesKeys[i] && string.IsNullOrWhiteSpace(textboxesKeys[i].Text))
+					textboxesKeys[i].Text = textboxesWithInitialWatermarkText[textboxesKeys[i]];
+		}
+
+		private bool ValidateAll()
+		{
+			foreach (var tb in textboxesWithInitialWatermarkText.Keys)
+				if (tb.Text == textboxesWithInitialWatermarkText[tb] || string.IsNullOrWhiteSpace(tb.Text))
+				{
+					UserMessages.ShowWarningMessage("Invalid path for " + tb.Tag.ToString());
+					tb.Focus();
+					return false;
+				}
+			if (numericUpDownStartSvnRevisionNumber.Value < 0)
+			{
+				UserMessages.ShowWarningMessage("Enter valid valud for " + numericUpDownStartSvnRevisionNumber.Tag.ToString());
+				numericUpDownStartSvnRevisionNumber.Focus();
+				return false;
+			}
+			if (textBoxSvnUrl.Text.StartsWith("svn://localhost", StringComparison.InvariantCultureIgnoreCase)
+				&& Process.GetProcessesByName("svnserve").Length == 0)
+			{
+				UserMessages.ShowWarningMessage("SvnUrl starts with 'svn://localhost', but no svnserve Processes found, this command will hence fail, please start svnserve first."
+					+ Environment.NewLine
+					+ Environment.NewLine + @"Most likely the svnserve.exe will sit in ProgramFiles\TortoiseSVN\bin or ProgramFile\Subversion\bin"
+					+ Environment.NewLine
+					+ Environment.NewLine + @"Working example is:"
+					+ Environment.NewLine + @"svnserve.exe --daemon --root c:\francois\dev\repos\vsprojects"
+					+ Environment.NewLine + "And then svn url can be:"
+					+ Environment.NewLine + "svn://localhost/windowsstartupmanager/trunk");
+				return false;
+			}
+			if (checkBoxInitRepo.Checked)//If requested to INIT the Git repo, check not already existing
+			{
+				string gitReportToInit = textBoxRemoteGitRepo.Text;
+				if (gitReportToInit.Contains("://"))
+				{
+					UserMessages.ShowWarningMessage("Found the text '://' for the Remote Git repo, meaning  the repository is not local, not currently supported to init a remote git repo");
+					return false;
+				}
+				if (Directory.Exists(gitReportToInit) && (Directory.GetFiles(gitReportToInit).Length > 0 || Directory.GetDirectories(gitReportToInit).Length > 0))
+				{
+					UserMessages.ShowWarningMessage("The Remote Git repo to INIT is a local directory but it is not empty, please ensure it is empty or the directory does not exist.");
+					return false;
+				}
+				if (!Directory.Exists(gitReportToInit))
+				{
+					try
+					{
+						Directory.CreateDirectory(gitReportToInit);
+						if (!Directory.Exists(gitReportToInit))
+						{
+							UserMessages.ShowWarningMessage("Unable to create Remote Git repo directory, cannot continue: " + gitReportToInit);
+							return false;
+						}
+					}
+					catch (Exception exc)
+					{
+						UserMessages.ShowWarningMessage("Unable to create Remote Git repo directory, cannot continue: " + exc.Message);
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		bool isbusy = false;
+		private void buttonAccept_Click(object sender, EventArgs e)
+		{
+			if (!ValidateAll())
+				return;
+
+			if (isbusy)
+			{
+				UserMessages.ShowWarningMessage("Please wait, already busy.");
+				return;
+			}
+
+			isbusy = true;
+			ThreadingInterop.DoAction(delegate
+			{
+				string remoteName = Path.GetFileNameWithoutExtension(textBoxRemoteGitRepo.Text);
+
+				if (!Directory.Exists(textBoxLocalGitClonedFolder.Text))
+					Directory.CreateDirectory(textBoxLocalGitClonedFolder.Text);
+
+				List<string> commandListArguments = new List<string>();
+
+				/*commandListArguments.Add(string.Format("svn init \"{0}\" \"{1}\"", textBoxSvnUrl.Text, textBoxLocalGitClonedFolder.Text));
+				commandListArguments.Add(string.Format("svn fetch"));*/
+				
+				commandListArguments.Add(string.Format("svn clone{0} -r{1}:HEAD \"{2}\" \"{3}\"", checkBoxStandardLayout.Checked ? " -s" : "", numericUpDownStartSvnRevisionNumber.Value, textBoxSvnUrl.Text, textBoxLocalGitClonedFolder.Text));
+
+				if (checkBoxInitRepo.Checked)
+					commandListArguments.Add(string.Format("init --bare \"{0}\"", textBoxRemoteGitRepo.Text));
+				//git remote add [-t <branch>] [-m <master>] [-f] [--tags|--no-tags] [--mirror=<fetch|push>] <name> <url>
+				commandListArguments.Add(string.Format("remote add \"{0}\" \"{1}\"", remoteName, textBoxRemoteGitRepo.Text));
+				commandListArguments.Add(string.Format("push \"{0}\" master", remoteName));
+
+				bool allSuccess = true;
+				foreach (var args in commandListArguments)
+					if (!RunGitCommand(args, textBoxLocalGitClonedFolder.Text))
+					{
+						allSuccess = false;
+						break;
+					}
+
+				if (allSuccess)
+					Process.Start("explorer", textBoxLocalGitClonedFolder.Text);
+
+				isbusy = false;
+			},
+			false);
+		}
+
+		private bool RunGitCommand(string arguments, string workingDir)
+		{
+			int exitCode;
+			bool result = ProcessesInterop.StartAndWaitProcessRedirectOutput(
+				new System.Diagnostics.ProcessStartInfo(GitExePath, arguments) { WorkingDirectory = workingDir },
+				(obj, output) => { if (output != null) AppendMessage(output); },
+				(obj, error) => { if (error != null) AppendMessage("ERROR: " + error); },
+				out exitCode);
+			if (result) AppendMessage("SUCCESS: arguments = " + arguments);
+			return result;
+		}
+
+		private void AppendMessage(string message)
+		{
+			Action<string> appendAction = (ms) =>
+			{
+				textBoxMessages.Text += GetCurrentDateString() + ms + Environment.NewLine;
+				textBoxMessages.SelectionStart = textBoxMessages.Text.Length;
+				textBoxMessages.SelectionLength = 0;
+				textBoxMessages.ScrollToCaret();
+			};
+			if (this.InvokeRequired)
+				this.Invoke(appendAction, message);
+			else
+				appendAction(message);
+		}
+
+		private string GetCurrentDateString()
+		{
+			return "[" + DateTime.Now.ToString("HH:mm:ss") + "] ";
+		}
+	}
+}
